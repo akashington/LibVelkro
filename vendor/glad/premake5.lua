2ab8dfb250316b5985abbf7a89b12caca16a8c77
Akash Patikkal Jnanesh <akash@velkro.org>
2026-01-11 00:08:46 +1100

    Fix bin/bin-int directory

--- vendor/glad/premake5.lua
+++ vendor/glad/premake5.lua
@@ -3,8 +3,8 @@ project "Glad"
     language "C"
     architecture "x64"
 
-    targetdir ("bin/%{prj.name}/%{cfg.architecture}-%{cfg.buildcfg}")
-    objdir ("bin-int/%{prj.name}/%{cfg.architecture}-%{cfg.buildcfg}")
+    targetdir ("../../bin/%{prj.name}/%{cfg.architecture}-%{cfg.buildcfg}")
+    objdir ("../../bin-int/%{prj.name}/%{cfg.architecture}-%{cfg.buildcfg}")
 
     files { "src/**.c", "include/glad/**.h", "include/KHR/**.h" }
 
